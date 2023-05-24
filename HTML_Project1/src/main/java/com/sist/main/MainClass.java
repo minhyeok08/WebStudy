49ce23d60c920d151879f9b756a86857210e4118
SIST <SIST@DESKTOP-360I4IK>
2023-05-24 18:15:46 +0900

    111

---
+++ HTML_Project1/src/main/java/com/sist/main/MainClass.java
@@ -0,0 +1,14 @@
+package com.sist.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try
+		{
+			
+			
+		}catch(Exception ex) {}
+	}
+
+}
