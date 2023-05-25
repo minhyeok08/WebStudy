58db967ec20455b3c555f915bdbcaed5c4fa89eb
SIST <SIST@DESKTOP-360I4IK>
2023-05-25 17:36:49 +0900

    1111

--- HTML_Project1/src/main/java/com/sist/dao/BoardDAO.java
+++ HTML_Project1/src/main/java/com/sist/dao/BoardDAO.java
@@ -138,8 +138,104 @@ public class BoardDAO {
 		return total;
 	}
 	//5-2. 상세보기 => 조회수 증가 (UPDATE) , 상세볼 게시물 읽기 (SELECT)
+	public BoardVO boardDetailData(int no)
+	{
+		BoardVO vo=new BoardVO();
+		try
+		{
+			getConnection();
+			String sql="UPDATE freeboard SET "
+					+"hit=hit+1 "
+					+"WHERE no="+no;
+			ps=conn.prepareStatement(sql);
+			ps.executeUpdate();
+			
+			sql="SELECT no,name,subject,content,TO_CHAR(regdate,'yyyy-MM-dd'),hit "
+				+"FROM freeboard "
+				+"WHERE no="+no;
+			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			vo.setNo(rs.getInt(1));
+			vo.setName(rs.getString(2));
+			vo.setSubject(rs.getString(3));
+			vo.setContent(rs.getString(4));
+			vo.setDbday(rs.getString(5));
+			vo.setHit(rs.getInt(6));
+			
+			rs.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			disConnection();
+		}
+		return vo;
+		
+	}
 	//5-3. 게시물 등록 => INSERT
+	// 용도 (SQL문장 사용법, HYML 태그 => 웹 사이트)
+	public void boardInsert(BoardVO vo)
+	{
+		try
+		{
+			getConnection();
+			String sql="INSERT INTO freeboard(no,name,subject,content,pwd) "
+					+"VALUES(fb_no_seq.nextval,?,?,?,?)";
+			ps=conn.prepareStatement(sql);
+			// 실행 요청 전에 ?에 값을 채워 줘야함
+			ps.setString(1, vo.getName());
+			ps.setString(2, vo.getSubject());
+			ps.setString(3, vo.getContent());
+			ps.setString(4, vo.getPwd());
+			
+			// 실행
+			ps.executeUpdate();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			disConnection();
+		}
+	}
 	//5-4. 수정 (UPDATE) => 먼저 입력된 게시물 읽기 , 실제 수정 (비밀번호 검색)
 	//5-5. 삭제 (DELETE) => 비밀번호 검색
+	public boolean boardDelete(int no, String pwd)
+	{
+		boolean bCheck=false; // 비밀번호 => 본인 여부 확인
+		try
+		{
+			getConnection();
+			String sql="SELECT pwd FROM freeboard "
+						+"WHERE no="+no;
+			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			rs.next();
+			String db_pwd=rs.getString(1);
+			rs.close();
+			
+			if(db_pwd.equals(pwd))
+			{
+				bCheck=true;
+				// 삭제
+				sql="DELETE FROM freeboard "
+					+"WHERE no="+no;
+				ps=conn.prepareStatement(sql);
+				ps.executeUpdate();
+			}
+		}catch(Exception ex)
+		{
+				ex.printStackTrace();
+		}
+		finally
+		{
+			
+		}
+		return bCheck;
+	}
 	//5-6. 찾기 (이름, 제목, 내용) => LIKE
 }
