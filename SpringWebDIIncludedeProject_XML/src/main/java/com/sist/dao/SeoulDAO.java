f003f7770b5752b8aa214fdb71d60c7a5b3de13f
minhyeok08 <ssklas08@gmail.com>
2023-07-31 17:19:35 +0900

    DI include 이용한 카테고리 분류별 정보출력

---
+++ SpringWebDIIncludedeProject_XML/src/main/java/com/sist/dao/SeoulDAO.java
@@ -0,0 +1,61 @@
+package com.sist.dao;
+
+import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.sist.mapper.*;
+import com.sist.vo.*;
+import java.util.*;
+@Repository
+public class SeoulDAO {
+	@Autowired
+	private SeoulMapper mapper;
+	
+	/*
+	@Select("SELECT no,title,poster,num "
+			+ "FROM (SELECT no,title,poster,rownum as num " 
+			+ "FROM (SELECT no,title,poster "
+			+ "FROM seoul_location ORDER BY no ASC)) "
+			+ "WHERE num BETWEEN #{start} AND #{end}")*/
+	
+	public List<SeoulVO> seoulLocationListData(Map map)
+	{
+		return mapper.seoulLocationListData(map);
+	}
+	@Select("SELECT CEIL(COUNT(*)/12.0) FROM seoul_location")
+	
+	public int seoulLocationTotalPage()
+	{
+		return mapper.seoulLocationTotalPage();
+	}
+	
+	/*@Select("SELECT no,totle,poster,num "
+			+ "FROM (SELECT no,title,poster,rownum as num "
+			+ "FROM (SELECT no,title,poster "
+			+ "FROM seoul_nature OREDEY BY no ASC)) "
+			+ "WHERE num BETWEEN #{start} AND #{end}")*/
+	public List<SeoulVO> seoulNatureListData(Map map)
+	{
+		return mapper.seoulNatureListData(map);
+	}
+	/*@Select("SELECT CEIL(COUNT(*)/12.0 FROM seoul_location")*/
+	public int seoulNatureTotalPage()
+	{
+		return mapper.seoulNatureTotalPage();
+	}
+	
+	/*@Select("SELECT no,title,poster,num "
+			+ "FROM (SELECT no,title,poster,rownum as num "
+			+ "FROM (SELECT no,title,poster "
+			+ "FROM seoul_shop ORDER BY no ASC)) "
+			+ "WHERE num BETWEEN #{start} AND #{end}")*/
+	public List<SeoulVO> seoulShopListData(Map map)
+	{
+		return mapper.seoulShopListData(map);
+	}
+	/*@Select("SELECT CEIL(count(*)/12.0) FROM seoul_nature")*/
+	public int seoulShopTotalPage()
+	{
+		return mapper.seoulShopTotalPage();
+	}
+}
---
+++ SpringWebDIIncludedeProject_XML/src/main/webapp/databoard/list.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div class="row">
+		<h1 class="text-center">자료실</h1>
+		<table class="table">
+		 <tr>
+		 	<td><a href="../databoard/insert.do" class="btn btn-sm btn-warning">새글</a></td>
+		 </tr>
+			<tr>
+				<th width=10% class="text-center">번호</th>
+				<th width=45% class="text-center">제목</th>
+				<th width=15% class="text-center">이름</th>
+				<th width=20% class="text-center">작성일</th>
+				<th width=10% class="text-center">조회수</th>
+			</tr>
+			<c:forEach var="vo" items="${list }">
+			<tr>
+				<th width=10% class="text-center">${vo.no }</th>
+				<th width=45%>${vo.subject }</th>
+				<th width=15% class="text-center">${vo.name }</th>
+				<th width=20% class="text-center">${vo.dbday }</th>
+				<th width=10% class="text-center">${vo.hit }</th>
+			</tr>
+			</c:forEach>
+		</table>
+		<table class="table">
+			<tr>
+				<td class="text-left">
+					Search:<select name=fs class="input-sm">
+						<option value="name">이름</option>
+						<option value="subject">제목</option>
+						<option value="content">내용</option>
+					</select>
+					<input type="text" name=ss class="input-sm" size=15>
+					<button class="btn btn-sm btn-success">검색</button>
+				</td>
+				<td class="text-right">
+					<a href="#" class="btn btn-sm btn-danger">이전</a>
+						${curpage } page / ${totalpage } pages
+					<a href="#" class="btn btn-sm btn-primary">다음</a>
+				</td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
